3e68bc389ae947347ba508e8eedfc76d2baa6e2e
AndreFCruz <andrecruz97@gmail.com>
2018-02-22 21:40:09 +0000

    refactored switch

--- TP2/src/Server.java
+++ TP2/src/Server.java
@@ -9,6 +9,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TimerTask;
 
+import jdk.nashorn.internal.ir.RuntimeNode.Request;
+
 public class PlatesServer implements Runnable {
 
     private static final int TIMEOUT = 10;
@@ -20,7 +22,13 @@ public class PlatesServer implements Runnable {
 
     TimerTask broadcastTask;
 
-    Map<String, String> database;
+    Map<String, String> database = new HashMap<>();
+
+    interface RequestHandler {
+        String handleRequest(String[] request);
+    }
+
+    Map<String, RequestHandler> handlers = new HashMap<>();
 
     private TimerTask makeMsgTask(String msg) {
         return new TimerTask() {
@@ -37,13 +45,25 @@ public class PlatesServer implements Runnable {
         this.serverPort = serverPort;
         this.mcastAddr = mcastAddr;
         this.mcastPort = mcastPort;
+
+        this.handlers.put("REGISTER", new RequestHandler() {
+            @Override
+            public String handleRequest(String[] request) {
+                return this.registerPlate(request);
+            }
+        });
+        this.handlers.put("LOOKUP", new RequestHandler() {
+            @Override
+            public String handleRequest(String[] request) {
+                return this.lookupPlate(request);
+            }
+        });
     }
 
     private void initialize() {
         this.socket = new DatagramSocket(serverPort);
         this.socket.setSoTimeout(TIMEOUT * 1000);
 
-        this.database = new HashMap<>();
         this.broadcastTask = makeMsgTask(Integer.toString(serverPort));
 
         System.out.println("Server initialized!");                
@@ -75,38 +95,12 @@ public class PlatesServer implements Runnable {
 
             String[] request = msg.split("\\s+");
             String response = new String();
-
-            switch (request[0]){
-                case "REGISTER":
-                    // to register the association of a plate number to the owners
-                    // Returns -1 if the plate number has already been registered;
-                    //  otherwise, returns the number of vehicles in the database.
-
-                    if (! isValidPlate(request[1]) || database.containsKey(request[1])) {
-                        System.out.println("The plate number has already been registered");
-                        response = "-1";
-                    } else {
-                        database.put(request[1],request[2]);
-                        System.out.println("The plate was registed sucessfully");
-                        response = Integer.toString(database.size());
-                    }
-                    break;
-                case "LOOKUP":
-                    // to get the owner of a given plate number.
-                    // Returns the owner's name or the string NOT_FOUND if the plate
-                    //  number was never registered.
-                    if (! isValidPlate(request[1]) || database.containsKey(request[1])) {
-                        System.out.println("The plate number exist in database");
-                        response = database.get(request[1]);
-                    } else {
-                        System.out.println("The plate number doesn't exist in database");
-                        response = "NOT_FOUND";
-                    }
-                    break;
-                default:
-                    response = "Invalid command";
-                    break;
+            if (handlers.containsKey(request[0])) {
+                response = handlers.get(request[0]).handleRequest(request);
+            } else {
+                response = "Invalid Request!";
             }
+
             byte[] sbuf = response.getBytes();
             DatagramPacket responsePkt = new DatagramPacket(sbuf, sbuf.length,
                                                             packet.getAddress(),
@@ -115,8 +109,41 @@ public class PlatesServer implements Runnable {
 
         }
 
-        close();
+        this.close();
     }
+
+    /**
+     * Returns the owner's name or the string NOT_FOUND if the plate
+     * number was never registered.
+     */
+	private String lookupPlate(String[] request) {
+		String response;
+        if (! isValidPlate(request[1]) || database.containsKey(request[1])) {
+            System.out.println("The plate number exist in database");
+            response = database.get(request[1]);
+        } else {
+            System.out.println("The plate number doesn't exist in database");
+            response = "NOT_FOUND";
+        }
+		return response;
+	}
+
+    /**
+     * Returns -1 if the plate number has already been registered;
+     * otherwise, returns the number of vehicles in the database.
+     */
+	private String registerPlate(String[] request) {
+		String response;
+        if (! isValidPlate(request[1]) || database.containsKey(request[1])) {
+            System.out.println("The plate number has already been registered");
+            response = "-1";
+        } else {
+            database.put(request[1],request[2]);
+            System.out.println("The plate was registed sucessfully");
+            response = Integer.toString(database.size());
+        }
+		return response;
+	}
 
     public static void main(String[] args) throws IOException {
         if (args.length != 3) {
