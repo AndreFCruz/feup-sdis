110d805973953a7677e708f2dacbfe5d39d4d42f
AndreFCruz <andrecruz97@gmail.com>
2018-03-26 19:13:11 +0100

    peer script

--- Proj1/peer.sh
+++ Proj1/peer.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-java -classpath bin service.Peer 1 224.0.0.0:8000 224.0.0.0:8001
-#java -classpath bin service.Peer 2 224.0.0.0:8000 224.0.0.0:8001
+if [ "$#" -ne 1 ]; then
+  echo "Usage: peer.sh <peer_num>"
+  exit 1
+fi
+
+java -Djava.net.preferIPv4Stack=true -classpath bin service.Peer "$1" 224.0.0.0:8000 224.0.0.0:8001
