258710ecd9f911c320ee240d1e1c35c0ce3fbbbb
AndreFCruz <andrecruz97@gmail.com>
2018-03-29 11:21:37 +0100

    Log class

--- Proj1/src/utils/Log.java
+++ Proj1/src/utils/Log.java
@@ -1,4 +1,17 @@
 package utils;
 
+import java.io.PrintStream;
+
 public class Log {
+    private static PrintStream logFile = System.err;
+
+    public static void logError(String msg) {
+        logFile.println("Error @ " + System.currentTimeMillis() + " : " + msg);
+        logFile.flush();
+    }
+
+    public static void logWarning(String msg) {
+        logFile.println("Warning @ " + System.currentTimeMillis() + " : " + msg);
+        logFile.flush();
+    }
 }
