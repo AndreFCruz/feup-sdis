3a1ad69d32b6c9a44501905b4725cfdf8e7d4d9d
AndreFCruz <andrecruz97@gmail.com>
2018-05-12 16:14:09 +0100

    ChordNode interface, mammamia

--- Proj2/src/network/ChordNode.java
+++ Proj2/src/network/ChordNode.java
@@ -1,5 +1,6 @@
 package network;
 
+import java.io.Serializable;
 import java.net.InetSocketAddress;
 
 /**
@@ -7,12 +8,68 @@ import java.net.InetSocketAddress;
  */
 public interface ChordNode {
 
+
+    /**
+     * Gets this node's id/key.
+     * @return this node's id/key.
+     */
+    int getId();
+
+    /**
+     * Gets this node's address (ip:port).
+     * @return this node's address.
+     */
+    InetSocketAddress getAddress();
+
+    /**
+     * Gets this node's successor.
+     * @return the address of this node's successor.
+     */
+    InetSocketAddress getSuccessor();
+
+    /**
+     * Gets this node's predecessor.
+     * @return the address of this node's predecessor.
+     */
+    InetSocketAddress getPredecessor();
+
+    /**
+     * Fetches the Object with the specified key.
+     * @param key the Object's key.
+     * @param <T> the type of the Object (must be serializable).
+     * @return the Object with the specified key, if found.
+     */
+    <T extends Serializable> T lookup(int key);
+
+    /**
+     * Puts the given object in this node's persistent memory.
+     * @param key the Object's key.
+     * @param <T> the type of the Object (must be serializable).
+     * @return the previous Object associated with key, or null if there
+     *  was no mapping for the given key.
+     */
+    <T extends Serializable> T put(int key);
+
+    /**
+     * Gets the ith finger on this node's finger table.
+     * The ith entry of node n will contain:
+     *  successor((n + 2^(i-1)) mod 2^m), m = 32
+     * @param i the finger's position in this node's finger table.
+     * @return the finger's address.
+     */
+    InetSocketAddress getIthFinger(int i);
+
     /**
      * Join a Chord Ring through the provided contact Node.
      * @param contact the address of the join contact.
      * @return whether join was successful.
      */
     boolean join(InetSocketAddress contact);
+
+    /**
+     * Causes this node to leave the Chord ring.
+     */
+    void leave();
 
     /**
      * Notify the given node that this node is its' predecessor.
@@ -29,10 +86,13 @@ public interface ChordNode {
     boolean notified(InetSocketAddress predecessor);
 
     /**
-     * Find id's successor.
-     * @param id a key in the hash table.
+     * Find key's successor (the node responsible for the given key/id).
+     * A successor of key is the first node whose id equals or follows key.
+     * Every key is stored in its successor node.
+     * @param key a key in the hash table.
      * @return the corresponding address.
      */
-    InetSocketAddress findSuccessor(int id);
+    InetSocketAddress findSuccessor(int key);
+
 
 }
