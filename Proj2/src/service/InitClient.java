04861ececfc9ee0225f6013928872ebe7f170f79
AndreFCruz <andrecruz97@gmail.com>
2018-05-24 19:37:56 +0100

    Starting InitClient (TestApp)

--- Proj2/src/service/InitClient.java
+++ Proj2/src/service/InitClient.java
@@ -1,4 +1,72 @@
 package service;
 
-public class InitClient {
+import network.RemotePeer;
+
+import java.nio.file.Paths;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.util.HashMap;
+import java.util.Map;
+
+
+// TODO finish implementing this class
+public class InitClient implements Runnable {
+
+    private Map<String, Runnable> handlers;
+    private RemotePeer stub;
+
+    private String action;
+    private String operand;
+
+    public static void main(String[] args) {
+        if (args.length < 2 || args.length > 3) {
+            System.out.println("Usage: java InitClient <peer_address> <action> <operand>");
+            return;
+        }
+
+        //host:port/name
+        String[] peer_ap = Utils.parseRMI(false, args[0]);
+        if (peer_ap == null)
+            return;
+
+        String action = args[1];
+        String operand = args.length > 2 ? args[2] : null;
+
+        InitClient app = new InitClient(action, operand);
+        new Thread(app).start();
+    }
+
+    InitClient(String action, String operand) {
+        this.action = action;
+        this.operand = operand;
+
+        handlers = new HashMap<>();
+        handlers.put("STATUS", this::handleStatus);
+    }
+
+    @Override
+    public void run() {
+        initiateRMIStub();
+        handlers.get(action).run();
+    }
+
+    private void initiateRMIStub() {
+        try { // TODO getRegistry
+//            Registry registry = Utils.getRegistry(peer_ap);
+//            stub = (RemotePeer) registry.lookup(peer_ap[2]);
+        } catch (Exception e) {
+            System.err.println("Error when opening RMI stub");
+            e.printStackTrace();
+        }
+    }
+
+    void handleStatus() {
+        try {
+            System.out.println(stub.getStatus());
+        } catch (RemoteException e) {
+            System.err.println("Client exception: " + e.toString());
+        }
+    }
+
 }
+
--- Proj2/src/service/Utils.java
+++ Proj2/src/service/Utils.java
@@ -1,4 +1,59 @@
 package service;
 
-public class Utils {
+import network.Log;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Utils {
+    // TODO mudar o parseRMI, nao precisa do ultimo digito
+    static String[] parseRMI(boolean Server, String accessPoint) {
+        Pattern rmiPattern;
+        if (Server) {
+            rmiPattern = Pattern.compile("//([\\w.]+)(?::(\\d+))?/(\\w+)?");
+        } else {
+            rmiPattern = Pattern.compile("//([\\w.]+)(?::(\\d+))?/(\\w+)");
+        }
+
+        Matcher m = rmiPattern.matcher(accessPoint);
+        String[] peer_ap = null;
+
+        if (m.find()) {
+            peer_ap = new String[]{m.group(1), m.group(2), m.group(3)};
+        } else {
+            Log.logError("Invalid Access Point!");
+        }
+
+        return peer_ap;
+    }
+
+    static Registry getRegistry(String[] serviceAccessPoint) {
+        Registry registry = null;
+        // Bind the remote object's stub in the registry
+
+        try {
+            if (serviceAccessPoint[1] == null) {
+                if (serviceAccessPoint[0] == "localhost") {
+
+                    registry = LocateRegistry.getRegistry();
+
+                } else {
+                    registry = LocateRegistry.getRegistry(serviceAccessPoint[0]);
+                }
+            } else {
+                if (serviceAccessPoint[0] == "localhost") {
+                    registry = LocateRegistry.getRegistry(serviceAccessPoint[1]);
+                } else {
+                    registry = LocateRegistry.getRegistry(serviceAccessPoint[0], Integer.parseInt(serviceAccessPoint[1]));
+                }
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        return registry;
+    }
 }
